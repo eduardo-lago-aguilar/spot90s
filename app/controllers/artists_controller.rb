47c17c86877df56e56234fa6c21493c70a8f099a
Eduardo Lago <ed@aleph.engineering>
2017-03-01 11:57:27 -0500

    #search action in ArtistsController done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,14 @@
 class ArtistsController < ApplicationController
+
+  def search
+    render json: reformat(perform_search(params[:query]))
+  end
+
+  def perform_search(query)
+
+  end
+
+  def reformat(artists)
+
+  end
 end
--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -1,3 +1,15 @@
 describe ArtistsController, type: :controller do
 
+  describe '#search' do
+    let(:artists) { [Faker::Name.name, Faker::Name.name, Faker::Name.name] }
+    let(:formatted_artists) { [Faker::Name.name, Faker::Name.name, Faker::Name.name] }
+    let(:query) { Faker::Name.name }
+
+    it 'performs a search on spotify and reformat the results' do
+      expect(controller).to receive(:perform_search).with(query).and_return artists
+      expect(controller).to receive(:reformat).with(artists).and_return formatted_artists
+      get :search, params: {query: query}
+      expect(response.body).to eq formatted_artists.to_json
+    end
+  end
 end
